20414865ad5c5f699ae8a80455797fe0ede5f1b9
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2022-09-08 09:35:01 -0400

    [CREATE] Setup of table view

---
+++ InfoNews/Support/UI/DataSource/TableSection.swift
@@ -0,0 +1,57 @@
+//
+//  TableSection.swift
+//  InfoNews
+//
+//  Created by Vinicius Galhardo Machado on 08/09/22.
+//
+
+import Foundation
+import UIKit
+
+protocol TableSectionDelegate: AnyObject {
+    func didSelect(at: Int)
+}
+
+class TableSection<T: UITableViewCell & CellProtocol>: TableSectionProtocol {
+    
+    // MARK: - Private properties
+    
+    private var viewModels: [T.ViewModel]
+    private weak var delegate: TableSectionDelegate?
+    
+    // MARK: - Init
+    
+    init(viewModels: [T.ViewModel], delegate: TableSectionDelegate? = nil) {
+        self.viewModels = viewModels
+        self.delegate = delegate
+    }
+    
+    var itemsCount: Int {
+        viewModels.count
+    }
+    
+    var cellIdentifier: String {
+        String(describing: T.self)
+    }
+    
+    var cellType: UITableViewCell.Type {
+        T.self
+    }
+    
+    func bindCell(cell: UITableViewCell, at row: Int) {
+        if let cell = cell as? T, row < itemsCount {
+            cell.bindIn(viewModel: viewModels[row])
+        }
+    }
+    
+    func didSelect(at: Int) {
+        delegate?.didSelect(at: at)
+    }
+}
+
+// MARK: - TableSectionDelegate
+
+extension TableSectionDelegate {
+    
+    func didSelect(at: Int) { }
+}
